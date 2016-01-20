02d026bf0697a24941f92defea2b2b428533c728
thoba <lose.thoba@gmail.com>
2016-01-20 21:07:00 +0200

    Defining nodes and properties.

---
+++ model.py
@@ -0,0 +1,22 @@
+from neomodel import (StructuredNode, StringProperty, DateTimeProperty, DateProperty, IntegerProperty)
+
+class Author(StructuredNode):
+  print 'Class for Author nodes'
+  # Names shoud not be duplicate (Unique_Index=True)
+  # http://neo4j.com/docs/stable/query-constraints.html
+  name = StringProperty(Unique_Index=True, required=True)
+  born = DateProperty()
+  died = DateProperty()
+
+
+class Book(StructuredNode):
+  print 'Class for Book nodes'
+
+  title = StringProperty(Unique_Index=True, required=True)
+  published = DateProperty()
+
+class Reader(StructuredNode):
+  print 'Class for Reader nodes'
+
+  name = StringProperty(Unique_Index=True, required=True)
+  born = DateProperty()
