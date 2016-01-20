4b9ce5cc3b83e218bbad85cbdfbe9f5748fa0491
thoba <lose.thoba@gmail.com>
2016-01-20 21:34:06 +0200

    PEP8

--- model.py
+++ model.py
@@ -1,22 +1,24 @@
-from neomodel import (StructuredNode, StringProperty, DateTimeProperty, DateProperty, IntegerProperty)
+from neomodel import (StructuredNode, StringProperty, DateProperty)
+
 
 class Author(StructuredNode):
-  print 'Class for Author nodes'
-  # Names shoud not be duplicate (Unique_Index=True)
-  # http://neo4j.com/docs/stable/query-constraints.html
-  name = StringProperty(Unique_Index=True, required=True)
-  born = DateProperty()
-  died = DateProperty()
+    print 'Class for Author nodes'
+    # Names shoud not be duplicate (Unique_Index=True)
+    # http://neo4j.com/docs/stable/query-constraints.html
+    name = StringProperty(Unique_Index=True, required=True)
+    born = DateProperty()
+    died = DateProperty()
 
 
 class Book(StructuredNode):
-  print 'Class for Book nodes'
+    print 'Class for Book nodes'
 
-  title = StringProperty(Unique_Index=True, required=True)
-  published = DateProperty()
+    title = StringProperty(Unique_Index=True, required=True)
+    published = DateProperty()
+
 
 class Reader(StructuredNode):
-  print 'Class for Reader nodes'
+    print 'Class for Reader nodes'
 
-  name = StringProperty(Unique_Index=True, required=True)
-  born = DateProperty()
+    name = StringProperty(Unique_Index=True, required=True)
+    born = DateProperty()
