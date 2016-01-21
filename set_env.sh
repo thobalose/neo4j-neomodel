3bfd6b4819d4ebfa7c181f73d25d18c4a8225b3f
thoba <lose.thoba@gmail.com>
2016-01-21 08:49:47 +0200

    Minor update.

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #Set the location of neo4j via an environment variable.
 
-echo "Hello, $USER! We hope you have a running instance of Neo4j in you machine."
+echo "Hello, $USER! We hope you have a running instance of Neo4j on you machine."
 echo -n 'Please provide us with you Neo4j USERNAME: '
 read username
 echo -n 'Please provide us with you Neo4j PASSWORD: '
 read password
 
 export NEO4J_REST_URL=http://$username:$password@localhost:7474/db/data/
+
+python main.py
