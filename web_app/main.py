5b37ac39fb8a302be7bd99005b295c125cf7b492
thoba <lose.thoba@gmail.com>
2016-03-04 15:12:27 +0200

    Minor updates.

--- web_app/main.py
+++ web_app/main.py
@@ -1,19 +1,15 @@
 from flask import Flask, request, render_template, Response, make_response
-
 from model import *
 from ctrlmodel import *
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     node = None
     if request.method == 'POST':
         text = request.form['text']
-        print text
-        node = searchNodes(str(text))
-
+        node = searchNodes(text)
     return render_template('index.html', node=node)
 
 if __name__ == '__main__':
